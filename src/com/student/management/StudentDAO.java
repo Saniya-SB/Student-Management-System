2fc9b548a51fc84671deccf72dad9ca9dd855bd6
Saniya-SB <saniya75555@gmail.com>
2026-08-02 01:04:04 +0530

    Implement add student feature

--- src/com/student/management/StudentDAO.java
+++ src/com/student/management/StudentDAO.java
@@ -1,4 +1,36 @@
 package com.student.management;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class StudentDAO {
+
+    public  void addStudent(Student student){
+        String sql = "INSERT INTO student(id,name,age,email,course,address) VALUES(?,?,?,?,?,?)";
+
+       try (        Connection con = DBConnection.getConnection();
+                    PreparedStatement ps = con.prepareStatement(sql);)
+       {
+           ps.setInt(1,student.getId());
+           ps.setString(2, student.getName());
+           ps.setInt(3,student.getAge());
+           ps.setString(4,student.getEmail());
+           ps.setString(5, student.getCourse());
+           ps.setString(6, student.getAddress());
+           ps.executeUpdate();
+
+           System.out.println("Record Insertion successful");
+
+       }
+       catch(SQLException e){
+           System.out.println("Inserting Record Unsuccessful");
+           e.printStackTrace();
+
+       }
+
+
+    }
+
+
 }
