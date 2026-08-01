fe7e0c0eb3233c87d02509c7b89dfa609adaa412
Saniya-SB <saniya75555@gmail.com>
2026-08-01 12:58:05 +0530

    Add Student model and verify database connection

--- src/com/student/management/Main.java
+++ src/com/student/management/Main.java
@@ -1,4 +1,15 @@
 package com.student.management;
 
+import java.sql.Connection;
+
 public class Main {
+    public static void main(String[] args) {
+        Connection con = DBConnection.getConnection();
+        if(con!=null){
+            System.out.println("Connection established successfully");
+        }
+        else{
+            System.out.println("Connection failed");
+        }
+    }
 }
